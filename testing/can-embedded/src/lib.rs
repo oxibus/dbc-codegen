54ae19ebbe2679399740b1e61786b1a63ada29c6
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2025-11-26 16:50:34 -0500

    chore: allow generated file to be included
    * Due to a compiler limitation, inner attributes are not allowed with the `include!` macro.  This PR removes all `#![...]` and `//! ...` from the generated code, which will result in some warnings being generated. We may need to add explicit `allow` to each individual function where relevant.
    * Do not place generated code inside the user's code - not a good practice.  All generated code should go into `OUT_DIR`.

--- testing/can-embedded/src/lib.rs
+++ testing/can-embedded/src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 
 #[rustfmt::skip]
-#[allow(clippy::pedantic)]
-#[allow(clippy::disallowed_names)]
-pub mod messages;
+#[expect(dead_code, unused_comparisons)]
+#[expect(clippy::excessive_precision, clippy::manual_range_contains, clippy::useless_conversion, clippy::absurd_extreme_comparisons, clippy::unnecessary_cast, clippy::disallowed_names)]
+mod messages {
+    include!(concat!(env!("OUT_DIR"), "/messages.rs"));
+}
